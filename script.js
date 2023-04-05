3807a60d86f8471f0fac3a53dd874021aca4190e
Matheus Raphael <mthsrphl@gmail.com>
2023-04-05 00:06:45 -0400

    [NOVO] #0 - Testando menu burguer

---
+++ script.js
@@ -0,0 +1,12 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu-section");
+console.log(menuSection)
+
+const  menuBurguer = menuSection?.querySelector(".menu-burguer");
+
+menuBurguer?.addEventListener("click", () =>{
+    menuSection?.classList.toggle("on", show);
+    show = !show;
+    console.log(menuBurguer)
+})
